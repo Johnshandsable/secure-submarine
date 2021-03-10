3e538533fbab328ac1302b9ef412319e191ed8b4
Johnshandsable <Jshands@asu.edu>
2021-03-10 16:02:27 -0600

    completed adding auth to secrets route and verifying auth

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,8 +2,13 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
+
   console.log('req.user:', req.user);
 
   pool
