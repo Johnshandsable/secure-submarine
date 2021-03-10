18b1a04f12d1fb7292121bf934aa33c5ff6a6c1b
Johnshandsable <Jshands@asu.edu>
2021-03-10 16:09:00 -0600

    added authentication for clearance levels

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -8,11 +8,12 @@ const {
 
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
-
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(
+      `SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`
+    )
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
